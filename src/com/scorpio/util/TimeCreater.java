a676c1cfb0d9034831fd96b8e71d8b44c046af56
lizhan <34084296+lizhan007@users.noreply.github.com>
2017-12-04 10:29:54 +0800

    test_commit

--- src/com/scorpio/util/TimeCreater.java
+++ src/com/scorpio/util/TimeCreater.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class TimeCreater {
 
 	public static String creat() {
-		SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmm");
+		SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
 		String date =sf.format(new Date());
 		return date;
 	}
